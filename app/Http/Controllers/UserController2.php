5c6987e37928b93d1d1701cb9bfee911b8783228
AiceIceCream <manuelhumphrey921@gmail.com>
2023-05-19 23:59:50 -0700

    Partial

---
+++ app/Http/Controllers/UserController2.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request; //Handling http request from lumen
+use App\Models\User; //My Model
+use App\Traits\ApiResponser; //Standard API response
+use DB; // can be use if not using eloquent, you can use DB component in lumen
+use App\Services\User2Services;
+
+use Illuminate\Http\Response;
+
+Class UserController2 extends Controller {
+    use ApiResponser;
+
+    protected $primaryKey = 'userId';
+    private $request;
+
+    public function __construct(Request $request)
+    {
+       
+    }
+    public function getUsers()
+    {
+     
+    }
+
+    public function index(){
+       
+    }
+    
+    public function add(Request $request){
+   
+    }
+
+    public function update(Request $request, $id){
+   
+    }
+
+    public function delete($id){
+    
+    }
+
+    public function show($id){
+    
+    } 
